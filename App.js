c9d093dffcf23e72724ec63762fa83f346925c88
XuanSon00 <142807255+XuanSon00@users.noreply.github.com>
2023-10-22 22:08:50 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import RegisterPage from './Register/Register';
+
+
+function App() {
+   return(
+      <RegisterPage />
+   );
+}
+
+export default App;
---
+++ Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function sidebar(){
+    return  (
+        <div>
+        <span>Sidebar</span>
+        </div>
+        )
+}
+
+export default sidebar
